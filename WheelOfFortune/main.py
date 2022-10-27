98ac8eb8810bff731a1c0f7d996317eda48f451f
Ryan Smith <rysmith2113@gmail.com>
2022-10-26 23:22:31 -0400

    Add a way to make the wheel spin

--- WheelOfFortune/main.py
+++ WheelOfFortune/main.py
@@ -5,37 +5,60 @@
 # COPYRIGHT: Copyright (c) 2022 by Ryan Smith
 #
 
-import pygame
+import pygame, time, math
+from typing import Tuple
 
 from window import Window
 from sprites.wheel import Wheel
 import colors
 
-def gameloop(running: bool) -> None:
-	if not running:
-		return
+def make_text(text: str, font: pygame.font.Font, use_aa: bool, x: int, y: int) -> Tuple[pygame.Surface, pygame.Rect]:
+	pass
 
 def main():
 	window = Window(1000, 1000)
 	window.set_background(255, 255, 255)
 
-	wheel = Wheel(250, 250, color=colors.WHITE, width=820, height=860)
+	wheel = Wheel(90, 70, color=colors.WHITE, width=820, height=860)
+
+	font = pygame.font.Font('freesansbold.ttf', 32)
+	
+	title_text = font.render('Wheel of Fortune', True, colors.BLACK)
+	
+	title_rect = title_text.get_rect()
+	title_rect.center = (0, 0)
+	title_rect = (350, 10)
 
 	all_sprites = pygame.sprite.Group()
 	all_sprites.add(wheel)
 
-	window.update()
-
 	running = True
-	while running:
-		#gameloop(running)
+	spinning = False
+	spin_end = 0
 
+	while running:
 		all_sprites.draw(window.surface())
 		pygame.display.flip()
+
+		window.surface().blit(title_text, title_rect)
+		window.update()
+
+		if spinning and (now := time.time()) <= spin_end:
+			pygame.draw.rect(window.surface(), colors.WHITE, (90, 70, 820, 860))
+			wheel.spin()
+			
+			if (seconds := pow(10, -math.floor(spin_end - now))) < 1:
+				time.sleep(seconds)
+			else:
+				spinning = False
 
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				running = False
+			elif event.type == pygame.KEYDOWN and event.key == pygame.K_s:
+				spinning = True
+				spin_end = time.time() + 5
+				
 
 	window.close()
 
--- WheelOfFortune/sprites/wheel.py
+++ WheelOfFortune/sprites/wheel.py
@@ -9,29 +9,33 @@ import os, pygame, random, time
 from typing import Tuple
 
 class Wheel(pygame.sprite.Sprite):
+	__ASSET_PATH = os.path.join("assets", "wheel.png")
+	__x: int
+	__y: int
+
 	def __init__(self, x: int, y: int, *, color: Tuple[int], width: int, height: int) -> None:
 		""" Create the wheel sprite. """
 		super().__init__()
 
+		self.__x, self.__y = x, y
+
 		# Load the wheel image from the assets folder
-		self.image = pygame.image.load(os.path.join("assets", "wheel.png")).convert_alpha()
+		self.image = pygame.image.load(self.__ASSET_PATH).convert_alpha()
 		self.rect = self.image.get_rect()
 		
 		# Center the wheel on the screen
 		self.image = pygame.transform.scale(self.image, (width, height))
-		self.rect.x, self.rect.y = 90, 70
+		self.rect.x, self.rect.y = x, y
+	
+	def rotate(self, angle: float, x: int, y: int) -> Tuple[pygame.Surface, pygame.Rect]:
+		rotated_image = pygame.transform.rotate(self.image, angle)
+		new_rect = rotated_image.get_rect(center = self.image.get_rect(center = (x, y)).center)
+		return rotated_image, new_rect
 	
 	def spin(self) -> int:
 		""" Spin the wheel and return the reward. """
 
-		random.seed(time.time())
-		
-
-		DEGREES = 15.0
-
-	
-	def rotate(self, angle: float) -> None:
-		""" Rotate the wheel a certain number of degrees. """
-
-		self.image = pygame.transform.rotate(self.image, angle)
-		self.rect
+		#random.seed(time.time())
+		img, rect = self.rotate(90, 500, 500)
+		self.image.fill((255, 255, 255, 0))
+		self.image, self.rect = img, rect
