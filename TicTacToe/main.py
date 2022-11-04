7e4b9ce2a5473029ecebddadbd9cd6089a5a0514
Ryan Smith <rysmith2113@gmail.com>
2022-11-03 23:28:12 -0400

    Add a way to check if the player won

--- TicTacToe/main.py
+++ TicTacToe/main.py
@@ -7,6 +7,41 @@
 
 from ui import *
 from tkinter import ttk, BOTH
+from typing import List
+
+def check_win(board_data: List[List[BoardSpot]], piece: str) -> bool:
+	# Convert the 2D array into a 1D array
+	linear_board = [piece.get_piece() for row in board_data for piece in row]
+
+	WIN_POSSIBILITIES = [
+		# Rows
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+
+		# Columns
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+
+		# Diagonals
+		[0, 4, 8],
+		[2, 4, 6]
+	]
+
+	for i in range(len(WIN_POSSIBILITIES)):
+		win = (
+			linear_board[WIN_POSSIBILITIES[i][0]] ==
+			linear_board[WIN_POSSIBILITIES[i][1]] ==
+			linear_board[WIN_POSSIBILITIES[i][2]] ==
+			piece
+		)
+
+		if win:
+			return True
+
+def move_is_valid(board_data: List[List[str]], row: int, column: int) -> bool:
+	return len(board_data[row][column]) == 0
 
 def main():
 	window = Window(title="Tic-Tac-Toe", height=620, width=600, theme="black")
@@ -20,13 +55,20 @@ def main():
 
 	ROWS = COLUMNS = 3
 
+	board_data: List[List[BoardSpot]] = [[], [], []]
+
 	# Add the board spaces
 	for row in range(ROWS):
 		for column in range(COLUMNS):
-			button = BoardSpot(board, textvariable=StringVar(), width=10, height=5)
+			button = BoardSpot(board, textvariable=StringVar(), command=None)
 			board.add_widget(button, row=row, column=column, ipadx=67, ipady=87)
+			board_data[row].append(button)
 
 	window.update()
+
+	current_turn = StringVar(Piece.X)
+	while not (check_win(Piece.X) or check_win(Piece.O)):
+		pass
 
 if __name__ == "__main__":
 	import sys
